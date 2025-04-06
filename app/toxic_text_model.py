96bf1cf88020e932d414aae834012fe8772988e0
Sukriti Deb <115589452+sukriti-d@users.noreply.github.com>
2025-04-06 09:52:30 +0530

    Update toxic_text_model.py

--- app/toxic_text_model.py
+++ app/toxic_text_model.py
@@ -1,7 +1,7 @@
 from transformers import pipeline
 
 # Lightweight model for toxicity detection
-classifier = pipeline("text-classification", model="mrm8488/distilroberta-finetuned-toxic-comments", top_k=None)
+classifier = pipeline("text-classification", model="unitary/toxic-bert", top_k=None)
 
 def detect_toxic_text(text):
     result = classifier(text)
